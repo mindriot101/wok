c1219cc7155519eb12f0f391793753192456eeb0
Simon Walker <s.r.walker101@googlemail.com>
2016-03-29 17:40:50 +0100

    Swap tabs for spaces

--- wok/contrib/hooks.py
+++ wok/contrib/hooks.py
@@ -59,13 +59,13 @@ class HeadingAnchors(object):
 
         sio_destination = StringIO()
 
-	# Use the extension of the template to determine the type of document 
-	if page.template.filename.endswith(".html") or page.filename.endswith(".htm"):
-        	logging.debug('[HeadingAnchors] outputting {0} as HTML'.format(page))
-	        tree.write(sio_destination, method='html')
-	else:
-        	logging.debug('[HeadingAnchors] outputting {0} as XML'.format(page))
-	        tree.write(sio_destination)
+        # Use the extension of the template to determine the type of document 
+        if page.template.filename.endswith(".html") or page.filename.endswith(".htm"):
+                logging.debug('[HeadingAnchors] outputting {0} as HTML'.format(page))
+                tree.write(sio_destination, method='html')
+        else:
+                logging.debug('[HeadingAnchors] outputting {0} as XML'.format(page))
+                tree.write(sio_destination)
         page.rendered = sio_destination.getvalue()
 
 
